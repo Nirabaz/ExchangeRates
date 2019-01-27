8f7354eb45e4dac79ec220b8fff5cd080e232a5d
Mykhailo Zabarin <mykhailo.zabarin@gmail.com>
2019-01-27 22:58:49 +0200

    first commit

---
+++ ExchangeRates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExchangeRates
+//
+//  Created by Mykhailo Zabarin on 1/27/19.
+//  Copyright © 2019 Mykhailo Zabarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
